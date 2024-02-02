4c4280c39f56dc6d678cdb92581b3bc6ca86ded0
kushalkumar1362 <kushalkumar1362@gmail.com>
2024-02-02 23:03:56 +0530

    Add Validation on user name

--- end.js
+++ end.js
@@ -2,26 +2,38 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = ` ${"Your Score : "} ${mostRecentScore}`;
 
-username.addEventListener('keyup', () => {
-  saveScoreBtn.disabled = !username.value;
+username.addEventListener('input', () => {
+  if (username.value.trim() !== "") {
+    username.style.border = "1px solid green";
+    name_error.style.display = "none";
+  }
 });
 
 saveHighScore = (e) => {
   e.preventDefault();
-
-  const score = {
+  if (username.value.trim() === "") {
+    username.style.border = "1px solid red";
+    name_error.style.display = "block";
+    username.focus();
+  } 
+  else {
+    username.style.border = "1px solid green";
+    name_error.style.display = "none";
+    const score = {
       score: mostRecentScore,
       name: username.value,
-  };
-  highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-  localStorage.setItem('highScores', JSON.stringify(highScores));
-  window.location.assign('/');
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+    return true;
+  }
 };
